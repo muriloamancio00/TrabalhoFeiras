69ee1fd71e01ce40c3dfb1e69241a107f1c07b5d
murilo <61051754+muriloamancio00@users.noreply.github.com>
2022-09-10 11:23:52 -0300

    types Administrador,Feirante,Usuario,migrats

--- database/migrations/2010_09_10_141454_create_permissions_table.php
+++ database/migrations/2010_09_10_141454_create_permissions_table.php
@@ -13,17 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('regra');
+            $table->boolean('permissao');
             $table->unsignedBigInteger('type_id');
             $table->foreign('type_id')->references('id')->on('types');
-            $table->rememberToken();
             $table->timestamps();
         });
+        
     }
 
     /**
@@ -33,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permissions');
     }
 };
